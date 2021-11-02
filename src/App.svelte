5b71bb7739b75a789b5eb037b187bac50a5e74ef
HasinduLanka <hlrathnayaka@gmail.com>
2021-11-02 09:48:09 +0530

    begin

--- src/App.svelte
+++ src/App.svelte
@@ -1,20 +1,18 @@
 <script lang="ts">
-	import ModeSwitcher from './ModeSwitcher.svelte';
-	import Tailwindcss from './Tailwindcss.svelte';
-	export let name: string;
+	import Tailwindcss from "./Tailwindcss.svelte";
 </script>
-<style>
-	.custom-style {
-		@apply italic;
-	}
-</style>
+
 <Tailwindcss />
-<ModeSwitcher />
+
 <main class="p-4 mx-auto text-center max-w-xl">
-	<h1 class="uppercase text-6xl leading-normal font-thin text-svelte">Hello {name}!</h1>
-	<p class="custom-style mt-[3rem]">
+	<h1 class="uppercase text-6xl leading-normal font-thin text-svelte">
+		Hello World!
+	</h1>
+	<p class="mt-[3rem]">
 		Visit the
-		<a href="https://svelte.dev/tutorial" class="text-blue-500 underline">Svelte tutorial</a>
+		<a href="https://svelte.dev/tutorial" class="text-blue-500 underline">
+			Svelte tutorial
+		</a>
 		to learn how to build Svelte apps.
 	</p>
 </main>
