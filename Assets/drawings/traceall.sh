594149e76968f96eb1de7d9ecf3c5afdd5a2ff02
HasinduLanka <hlrathnayaka@gmail.com>
2021-11-02 15:00:13 +0530

    trace svgsons

--- Assets/drawings/traceall.sh
+++ Assets/drawings/traceall.sh
@@ -1,5 +1,27 @@
-#!/bin/bash
+#!/bin/sh
+
+rm -rf svgs
+mkdir -p svgs
+
+echo "export default Drawings = {" >svgsons/drawings.js
+
 for filename in raw/*.*; do
-    sh trace.sh $filename
-#     echo $filename
+
+    FILE=${filename%%.*}
+    FILENAME=$(basename $FILE)
+
+    # -monochrome
+    convert -bordercolor black -border 1 $filename $FILE-tmp.bmp
+    potrace $FILE-tmp.bmp -s --opaque -o svgs/$FILENAME.svg
+
+    rm -f $FILE-tmp.bmp
+
+    echo -e '\n"'$FILENAME'"' : >>svgsons/drawings.js
+    svgson svgs/$FILENAME.svg -p >>svgsons/drawings.js
+    echo "," >>svgsons/drawings.js
+
+    echo "Traced $FILE"
+
 done
+
+echo -e "\n}" >>svgsons/drawings.js
